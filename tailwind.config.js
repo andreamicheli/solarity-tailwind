4f1167569681a89d09ce053819844c0baa6a82ac
andreamicheli <andymicheli2000@gmail.com>
2021-11-12 13:35:01 +0000

    caricare solaritty

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      dropShadow: {
+        'green': '15 25px 25px rgba(52, 211, 153, 0.25)',
+      },
+
+      // fontFamily: {
+      //   Roboto: 'sans-serif'
+      // }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
